331d7c0ed7ef9dd02cd3210b8da62e3cfc440619
José Domingo Muñoz <josedom24@gmail.com>
2015-04-14 12:20:10 +0200

    Paso A

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,12 +1,29 @@
-from bottle import route, default_app
+# -*- encoding: utf-8 -*-
+from bottle import default_app, get, post, template, request, static_file, response
+import requests
+from requests_oauthlib import OAuth1
+from urlparse import parse_qs
 
-@route('/name/<name>')
-def nameindex(name='Stranger'):
-    return '<strong>Hello, %s!</strong>' % name
+def get_request_token():
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+    )
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
  
-@route('/')
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+ 
+CONSUMER_KEY = "MYQ9JgvCMJdjcqtX4dRnk1G4S"
+CONSUMER_SECRET = "JeAiQ9IyFJdp3LWrBTl3EBbaqQgSuk0D1aP63JqGcq8lQxRa0c"
+
+TOKENS = {}
+
+@get('/')
 def index():
-    return '<strong>Hello World!</strong>'
+    get_request_token()
+
+
 
 # This must be added in order to do correct path lookups for the views
 import os
