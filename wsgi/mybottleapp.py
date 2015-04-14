de8b6dcccff9c6ec41b7f8d9e31a231e18fbb19f
José Domingo Muñoz <josedom24@gmail.com>
2015-04-14 12:33:07 +0200

    Paso B

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -4,12 +4,7 @@ import requests
 from requests_oauthlib import OAuth1
 from urlparse import parse_qs
 
-def get_request_token():
-    oauth = OAuth1(CONSUMER_KEY,
-                   client_secret=CONSUMER_SECRET,
-    )
-    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
- 
+
 REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
 AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
 ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
@@ -18,6 +13,18 @@ CONSUMER_KEY = "MYQ9JgvCMJdjcqtX4dRnk1G4S"
 CONSUMER_SECRET = "JeAiQ9IyFJdp3LWrBTl3EBbaqQgSuk0D1aP63JqGcq8lQxRa0c"
 
 TOKENS = {}
+
+def get_request_token():
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+    )
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+	credentials = parse_qs(r.content)
+	TOKENS["request_token"] = credentials.get('oauth_token')[0]
+	TOKENS["request_token_secret"] = credentials.get('oauth_token_secret')[0]
+ 	authorize_url = AUTHENTICATE_URL + TOKENS["request_token"]
+	return template('index.tpl', authorize_url=authorize_url)
+
 
 @get('/')
 def index():
