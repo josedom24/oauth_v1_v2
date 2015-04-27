c44cbed2b77819c591a1ed4564250186aba2999c
josedom24 <josedom24@gmail.com>
2015-04-27 22:30:21 +0200

    Token expirada

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -82,19 +82,15 @@ def tweet_submit():
 @get('/youtube')
 def info_youtube():
   token=request.get_cookie("token", secret='some-secret-key')
-  if token:
-      try:
-        oauth2 = OAuth2Session(client_id, token=token)
-        redirect("/perfil")
-      except TokenExpiredError as e:
-        response.set_cookie("token", '',max_age=0)
-        redirect("/youtube")
-      
-  else:
-      oauth2 = OAuth2Session(client_id, redirect_uri=redirect_uri,scope=scope)
-      authorization_url, state = oauth2.authorization_url('https://accounts.google.com/o/oauth2/auth')
-      response.set_cookie("oauth_state", state)
-      return "<a href='%s'>Perfil de youtube</a>" % authorization_url
+  try:
+    oauth2 = OAuth2Session(client_id, token=token)
+    redirect("/perfil")
+  except TokenExpiredError as e:
+    response.set_cookie("token", '',max_age=0)
+    oauth2 = OAuth2Session(client_id, redirect_uri=redirect_uri,scope=scope)
+    authorization_url, state = oauth2.authorization_url('https://accounts.google.com/o/oauth2/auth')
+    response.set_cookie("oauth_state", state)
+    return "<a href='%s'>Perfil de youtube</a>" % authorization_url
 
 @get('/google')
 def get_token():
