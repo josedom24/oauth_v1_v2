95759dbfa93e8a8a0e13a243a14c3a161908c1a9
josedom24 <josedom24@gmail.com>
2016-05-07 20:46:40 +0200

    Principal

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -36,7 +36,10 @@ def get_access_token(TOKENS):
                    resource_owner_key=TOKENS["request_token"],
                    resource_owner_secret=TOKENS["request_token_secret"],
                    verifier=TOKENS["verifier"],)
-	r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+	print "request_token:"+TOKENS["request_token"]
+  print "request_token_secret:"+TOKENS["request_token_secret"]
+  print "verifier:"+TOKENS["verifier"]
+  r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
   print r.status_code
 	credentials = parse_qs(r.content)
 	TOKENS["access_token"] = credentials.get('oauth_token')[0]
